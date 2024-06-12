a04cb9ccfc4363ab886906b145214b625f02e684
Rodrigo Sousa <rodrigoasferreira@protonmail.com>
2024-06-11 23:08:25 -0300

    Make tab work correctly
    instead of exporting a whole function, exporting the "html" of js
    seems to work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import pageHome from "./pageHome";
+import element from "./pageHome";
 import pageRecipes from "./pageRecipes";
 import pageAbout from "./pageAbout";
 
@@ -17,14 +17,23 @@ console.log("working???");
 
 // por exemplo quando for mudar algo no DOM faz no doThing() e dá um append aqui
 const content = document.getElementById("content");
-content.appendChild(pageHome());
-content.appendChild(pageRecipes());
+// content.appendChild(pageRecipes());
 
 const but1 = document.getElementById("but1");
 const but2 = document.getElementById("but2");
 const but3 = document.getElementById("but3");
 
-but1.addEventListener("click", () => {});
+// const pag1 = document.getElementById("home");
+// content.appendChild(pageHome());
+
+// const pagHomeFoda = document.getElementById("home");
+
+but1.addEventListener("click", () => {
+  element.classList.add("active");
+  content.appendChild(element);
+  // pag1.classList.add("active");
+  // pagHomeFoda.classList.add("active");
+});
 
 // but2.addEventListener("click", () => {});
 
--- src/pageHome.js
+++ src/pageHome.js
@@ -1,21 +1,28 @@
 function pageHome() {
-  console.log("initial page load!");
-
-  const element = document.createElement("div");
-  element.classList.add("tabcontent");
-  element.classList.add("active");
-  // element.id.add("home");
-  element.setAttribute("id", "home");
-
-  element.innerHTML = `
-      <h3>um dos restaurantes.</h3>
-      <p>a comida aqui é servida.</p>
-  `;
+  // console.log("initial page load!");
+  // const element = document.createElement("div");
+  // element.classList.add("tabcontent");
+  // // element.classList.add("active");
+  // element.setAttribute("id", "home");
+  // element.innerHTML = `
+  //     <h3>um dos restaurantes.</h3>
+  //     <p>a comida aqui é servida.</p>
+  // `;
   // element.classList.add("initial-page");
   // talvez pegue isso pra adicionar as tag e class e não fazer uma nova div
   // dentro de outra div! aí só tem que botar o <h3> e <p> dentro da div feita pelo js
-
-  return element;
+  // return element;
 }
 
-export default pageHome;
+const element = document.createElement("div");
+element.classList.add("tabcontent");
+// element.classList.add("active");
+element.setAttribute("id", "home");
+
+element.innerHTML = `
+  <h3>um dos restaurantes.</h3>
+  <p>a comida aqui é servida.</p>
+  <p>a bagaça do mady tá no bolso</p>
+`;
+
+export default element;
