5305b3bb50b0623b42d17586b878eaa5c4a85fcd
Rodrigo Sousa <rodrigoasferreira@protonmail.com>
2024-06-19 21:25:24 -0300

    Add image to website
    also remove some code i found inecessary in the css

--- src/pageHome.js
+++ src/pageHome.js
@@ -1,3 +1,5 @@
+import desgraca from "./cuscuzPaulista.jpg";
+
 const pageHome = document.createElement("div");
 pageHome.classList.add("tabcontent");
 pageHome.setAttribute("id", "home");
@@ -6,5 +8,11 @@ pageHome.innerHTML = `
   <h3>Um dos restaurantes</h3>
   <p>a comida aqui é servida.</p>
 `;
+
+// adiciona a imagem na div
+const cuscuzHorrivel = new Image();
+cuscuzHorrivel.src = desgraca;
+
+pageHome.appendChild(cuscuzHorrivel);
 
 export default pageHome;
